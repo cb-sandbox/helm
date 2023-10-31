b3837d9b0211cbba9c8f962ffea8ef5bf720a586
Bob Cauthen <89991256+CBCauthen@users.noreply.github.com>
2023-10-31 13:03:22 -0400

    Update workload_identity.sh
    replaced all occurrences of CLUSTER_PROJECT

--- scripts/workload_identity.sh
+++ scripts/workload_identity.sh
@@ -7,12 +7,12 @@ echo "----> Setting up Workload Identity"
 GCP_SA_NAME=jenkins-build-sa
 NAMESPACE=cloudbees-sda
 K8S_SA_NAME=jenkins
-CLUSTER_PROJECT=cb-thunder-v2
+# CLUSTER_PROJECT=cb-thunder-v2
 
 gcloud iam service-accounts add-iam-policy-binding \
   --role roles/iam.workloadIdentityUser \
-  --member "serviceAccount:$CLUSTER_PROJECT.svc.id.goog[$NAMESPACE/$K8S_SA_NAME]" \
-  $GCP_SA_NAME@$CLUSTER_PROJECT.iam.gserviceaccount.com
+  --member "serviceAccount:cb-thunder-v2.svc.id.goog[$NAMESPACE/$K8S_SA_NAME]" \
+  $GCP_SA_NAME@cb-thunder-v2.iam.gserviceaccount.com
 
 kubectl annotate serviceaccount -n $NAMESPACE $K8S_SA_NAME \
-  iam.gke.io/gcp-service-account=$GCP_SA_NAME@$CLUSTER_PROJECT.iam.gserviceaccount.com
+  iam.gke.io/gcp-service-account=$GCP_SA_NAME@cb-thunder-v2.iam.gserviceaccount.com
